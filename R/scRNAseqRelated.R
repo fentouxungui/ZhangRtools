c31412a7c41c45f1a6519dfd4daae3cef6cd5965
FenTouXunGui <763278134@qq.com>
2024-01-11 13:21:09 +0800

    scRNAseq related function added

---
+++ R/scRNAseqRelated.R
@@ -0,0 +1,29 @@
+#' 各个细胞内的高表达基因
+#'
+#' @description
+#' 对Seurat对象里的各个单细胞，分别统计top表达的基因，即基因的reads比例大于所设定的阈值expt.cut。
+#'
+#' @param SeuratObj Seurat对象
+#' @param expr.cut 针对UMI counts比例所设定的cut off，用于定义高表达的基因。
+#'
+#' @return A data.frame
+#' @export
+#'
+#' @examples
+#' # top_genes(SeuratObj = obj, expr.cut = 0.01)
+top_genes <- function(SeuratObj, expr.cut = 0.01){
+  require(parallel)
+  require(dplyr)
+  require(Seurat)
+  top.list <- mclapply(1:ncol(SeuratObj), function(x){
+    values <- sort(SeuratObj@assays$RNA@counts[,x],decreasing = TRUE)
+    rates <- values/sum(values)
+    top <- values[rates > 0.01]
+    data.frame(Gene = names(top), Expr = unname(top))
+  }, mc.cores = detectCores())
+  top.df <- Reduce(rbind, top.list)
+  group_by(top.df, Gene) %>%
+    summarise(mean = mean(Expr), median = median(Expr), Cells = length(Expr)) -> top.statics
+  return(top.statics)
+}
+
