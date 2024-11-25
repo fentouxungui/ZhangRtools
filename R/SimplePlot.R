bf3f09b14a28d16c86a86b9b42937180d6100c0f
FenTouXunGui <43262531+fentouxungui@users.noreply.github.com>
2024-11-25 10:36:15 +0800

    新加火山图风格一

--- R/SimplePlot.R
+++ R/SimplePlot.R
@@ -137,3 +137,57 @@ David_dotplot <- function(df,
     )
 }
 
+
+#' 火山图 风格一
+#'
+#' @param data a data.frame with columns: gene, pval, lfc, 默认lfc以2为底
+#' @param markers genes in gene column to be labeled
+#' @import ggplot2 dplyr ggrepel
+#' @return a plot
+#' @export
+#'
+#' @examples
+#' # null
+Volcano_plot_1 <- function(plot.data, markers, pvalue.cutoff = 0.05, lfc.cutoff = 1, x.lim = c(-10,10)){
+  requireNamespace("ggplot2")
+  requireNamespace("dplyr")
+  requireNamespace("ggrepel")
+
+  # Remove any rows that have NA as an entry
+  plot.data <- na.omit(plot.data)
+  markers.up <- markers[markers %in% plot.data$gene[plot.data$lfc > 0]]
+  markers.down <- markers[markers %in% plot.data$gene[plot.data$lfc < 0]]
+  # # Color the points which are up or down
+  # plot.data <- mutate(plot.data, color = case_when(plot.data$lfc > lfc.cutoff & plot.data$pval > -log10(pvalue.cutoff) ~ paste0("Increased (log2(FC) > ", lfc.cutoff, ")"),
+  #                                        plot.data$lfc < -lfc.cutoff & plot.data$pval > -log10(pvalue.cutoff) ~ paste0("Decreased (log2(FC) < -", lfc.cutoff,")"),
+  #                                        plot.data$lfc >= -lfc.cutoff |  plot.data$lfc <= lfc.cutoff | plot.data$pval < -log10(pvalue.cutoff) ~ "nonsignificant"))
+  # vol <- ggplot2::ggplot(plot.data, aes(x = lfc, y = pval, color = color))
+  # Make a basic ggplot2 object with x-y values
+  vol <- ggplot2::ggplot(plot.data, aes(x = lfc, y = pval))
+
+  # Add ggplot2 layers
+  vol +
+    ggtitle(label = "Volcano Plot", subtitle = "Markers are colored by fold-change direction") +
+    geom_hline(yintercept = log10(pvalue.cutoff), colour = "darkgrey") + # Add p-adj value cutoff line
+    geom_vline(xintercept = c(-lfc.cutoff, lfc.cutoff), colour = "darkgrey") + # Add p-adj value cutoff line
+    geom_point(size = 1.5, colour = "black", alpha = 0.1, na.rm = T) +
+    # 设置一下上下调的颜色
+    # upregulated markers
+    geom_point(data = plot.data[plot.data$gene %in% markers.up,],colour = "red",cex = 2) +
+    # geom_text(data = plot.data[plot.data$gene %in% markers.up,], aes(label = gene),colour = "blue",size = 4,position = position_dodge2(width = 1, padding = 0.5)) +
+    geom_text_repel(data = plot.data[plot.data$gene %in% markers.up,], aes(label = gene),colour = "red",size = 4, position = position_dodge2(width = 1, padding = 0.5)) +
+    # downregulated markers
+    geom_point(data = plot.data[plot.data$gene %in% markers.down,],colour = "blue",cex = 2) +
+    geom_text_repel(data = plot.data[plot.data$gene %in% markers.down,], aes(label = gene),colour = "blue",size = 4, position = position_dodge2(width = 1, padding = 0.5)) +
+    # scale_color_manual(name = "Directionality",values = c("Increased (log2(FC) > 1)"= "red", "Decreased (log2(FC) < -1)" = "blue", "nonsignificant" = "darkgray")) +
+    theme_bw(base_size = 14) + # change overall theme font size?
+    theme(legend.position = "right") + # change the legend
+    xlab(expression(log[2](case / control))) + # Change X-Axis label
+    ylab(expression(-log[10]("adjusted p-value"))) + # Change Y-Axis label
+    xlim(x.lim) +
+    scale_y_continuous(trans = "log1p") # Scale yaxis due to large p-values
+}
+
+
+
+
